d341e4128a2d570514084f5a88245b9957e120ef
SamFabra <samuel.fabra@ontariotechu.net>
2023-04-05 21:22:16 -0400

    Game over screen

---
+++ Assets/Scripts/GameEndTrigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEndTrigger : MonoBehaviour
+{
+
+    private PlayerBehaviour playerRef;
+    void Start()
+    {
+        playerRef = FindObjectOfType<PlayerBehaviour>();
+    }
+
+private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (playerRef.hasBlue == true && playerRef.hasRed == true && playerRef.hasGreen == true && playerRef.hasPurple == true)
+            {
+                SceneManager.LoadScene("Game Over!");
+            }
+        }
+    }
+
+    }
+
+
+
